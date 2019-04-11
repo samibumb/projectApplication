c23fe8cdbddac8757b96ae123abf75ad092aa121
Sami <bmb,sami@yahoo.com>
2019-04-11 13:14:29 +0300

    application in progress

--- src/main/Java/org/projectapp/DealerAuto.java
+++ src/main/Java/org/projectapp/DealerAuto.java
@@ -1,4 +1,4 @@
-package src.projectApp;
+package main.Java.org.projectapp;
 
 import java.util.ArrayList;
 import java.util.List;
--- src/main/Java/org/projectapp/Features.java
+++ src/main/Java/org/projectapp/Features.java
@@ -1,4 +1,4 @@
-package src.projectApp;
+package main.Java.org.projectapp;
 
 public interface Features {
 
