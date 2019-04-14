7f07a88f388d58e67b98693fece470a1a9332ad5
Sami <bmb,sami@yahoo.com>
2019-04-14 21:00:58 +0300

    application in progress

--- src/main/Java/org/projectapp/AppMain.java
+++ src/main/Java/org/projectapp/AppMain.java
@@ -2,50 +2,75 @@ package main.Java.org.projectapp;
 
 
 import java.util.Iterator;
+import java.util.Scanner;
 
 public class AppMain {
-    public static void main(String[] args){
+    public static void main(String[] args) throws Exception {
 
-        DealerAuto dealerAuto = new DealerAuto("AutoWorld" ,
-                "Cluj-Napoca/Romania" , "0264-436-905" , "autoworld@audi.com");
+        Scanner scanner = new Scanner(System.in);
+        int nrOfAttemps = 0, nrMaxAttemps = 3;
+        boolean guess = false;
 
-        System.out.println("Dealer "+dealerAuto.getName()+"\n"+"Address :"+dealerAuto.getAddress()+"\n"
-                +"Phone number :"+dealerAuto.getPhoneNumber()+"\n"+"Email :"+dealerAuto.geteMail()+"\n");
+        while (guess != true && nrOfAttemps < nrMaxAttemps) {
 
-        System.out.println(dealerAuto.getName()+" Dealer from "+dealerAuto.getAddress()+" has the following cars:");
+            System.out.println("Username and password :");
+            String userName = scanner.nextLine();
+            String password = scanner.nextLine();
 
-        VehicleComposition carElements = new VehicleComposition("4 Michelin wheels","Audi chassis" ,4
-        ,"VG Motors" , "CC system" , "8-Speed TipTronic Gearbox");
+            if (userName.equals("samibumb") && password.equals("samieboss")) {
+                guess = true;
+                DealerAuto dealerAuto = new DealerAuto("AutoWorld",
+                        "Cluj-Napoca/Romania", "0264-436-905", "autoworld@audi.com");
 
-        Vehicle vehicle1 = new Vehicle(1 , "Audi" , "Q8" , "Germany" ,
-                2018 , "Diesel" , 3.000 , 286 , "90.000€",carElements);
+                System.out.println("\nDealer " + dealerAuto.getName() + "\n" + "Address :" + dealerAuto.getAddress() + "\n"
+                        + "Phone number :" + dealerAuto.getPhoneNumber() + "\n" + "Email :" + dealerAuto.geteMail() + "\n");
 
-        Vehicle vehicle2 = new Vehicle(2 , "Audi" , "A7" , "Romania" ,
-                2018 , "Petrol" , 3.000 , 245 , "65.000€" ,carElements);
+                System.out.println(dealerAuto.getName() + " Dealer from " + dealerAuto.getAddress() + " has the following cars:");
+
+                VehicleComposition carElements = new VehicleComposition("4 Michelin wheels", "Audi chassis", 4
+                        , "VG Motors", "CC system", "8-Speed TipTronic Gearbox");
+
+                Vehicle vehicle1 = new Vehicle(1, "Audi", "Q8", "Germany",
+                        2018, "Diesel", 3.000, 286, "90.000€", carElements);
+
+                Vehicle vehicle2 = new Vehicle(2, "Audi", "A7", "Romania",
+                        2018, "Petrol", 3.000, 245, "65.000€", carElements);
 
 
-        Vehicle vehicle3 = new Vehicle(3 , "Audi" , "Q5" , " France" ,
-                2017 , "Diesel" , 2.000 , 190 , "50.000€" ,carElements);
+                Vehicle vehicle3 = new Vehicle(3, "Audi", "Q5", " France",
+                        2017, "Diesel", 2.000, 190, "50.000€", carElements);
 
-        dealerAuto.addVehicleToDealerShip(vehicle1);
-        dealerAuto.addVehicleToDealerShip(vehicle2);
-        dealerAuto.addVehicleToDealerShip(vehicle3);
+                dealerAuto.addVehicleToDealerShip(vehicle1);
+                dealerAuto.addVehicleToDealerShip(vehicle2);
+                dealerAuto.addVehicleToDealerShip(vehicle3);
 
-        Iterator iterator=dealerAuto.vehicleList.iterator();
-        while (iterator.hasNext()){
-            System.out.println(iterator.next());
+                Iterator iterator = dealerAuto.vehicleList.iterator();
+                while (iterator.hasNext()) {
+                    System.out.println(iterator.next());
+                }
+
+                dealerAuto.sell(vehicle1);
+                dealerAuto.getStock();
+
+                try {
+                    vehicle1.drive(55);
+                } catch (Exception e) {
+                    System.out.println("This happens when you violate the law " + e);
+                }
+
+            } else {
+                System.out.println("Invalid username or password.Please try again!");
+            }
+            nrOfAttemps++;
+
+
+        }
+        if (nrOfAttemps>nrMaxAttemps){
+            throw new Exception("Try again later");
         }
 
-        dealerAuto.sell(vehicle1);
-        dealerAuto.getStock();
-
-        try {
-           vehicle1.drive(55);
-       }catch (Exception e){
-           System.out.println("This happens when you violate the law "+e);
-       }
-
     }
+
 
 
 }
